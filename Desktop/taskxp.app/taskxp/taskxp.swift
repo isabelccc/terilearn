ad87800f6a6534c4610420d3d54b2f1fceaf9386
isabelccc <cxl0603@gmail.com>
2025-03-12 00:20:24 -0400

    Initial commit

---
+++ Desktop/taskxp.app/taskxp/taskxp.swift
@@ -0,0 +1,17 @@
+//
+//  taskxp_appApp.swift
+//  taskxp.app
+//
+//  Created by Lin L on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct taskxp_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
